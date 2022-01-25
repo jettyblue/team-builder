66ef687345db6b55d911aac77df49612f6b53437
unknown <lindsay.kelly15@gmail.com>
2022-01-25 18:40:41 -0500

    app.js functioning, mvp met

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const initialFormValues = {
 
 function App() {
   
-  const [member, setMember] = useState([]);
+  const [members, setMembers] = useState([]);
   const [formValues, setFormValues] = useState(initialFormValues);
   const [formError, setFormError] = useState('');
 
@@ -28,16 +28,47 @@ function App() {
       email: formValues.email.trim(),
       role: formValues.role
     }
+
+    if(!newMember.name || !newMember.email || !newMember.role) {
+      setFormError('All fields are required');
+    }
+
+    setMembers([newMember, ...members]);
+    setFormError('');
+    setFormValues(initialFormValues);
   }
-
-
 
   return (
     <div className="App">
-      {/* <h2>{error}</h2> */}
+      <h1>Team Members</h1>
+      <p>Fill out the form below and click "SUBMIT' to add a member to your team</p>
+      <h3 className='error-text'>{formError}</h3>
 
+      <MemberForm
+        formValues={formValues}
+        updateForm={updateForm}
+        submitForm={submitForm}
+      />
+
+      <p className='memberHeader'>Submission was accepted if your name appears below</p>
+      {
+        members.map(member => {
+          return (
+            <Member key={member.id} details={member} />
+          )
+        })
+      }
+
+      <p className='activeMemberHeader'>Active Members:</p>
+      {
+        members.map(member => {
+          return (
+            <Member key={member.name} details={member} />
+          )
+        })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
--- src/Components/MemberForm.js
+++ src/Components/MemberForm.js
@@ -1,31 +1,32 @@
 import React from 'react';
 
 export default function MemberForm(props) {
-    const { values, update, submit } = props;
+
+    const { formValues, updateForm, submitForm } = props;
 
     const onChange = evt => {
-        const name = evt.target.name;
-        const { value }  = evt.target;
+        const inputName = evt.target.name;
+        const inputValue = evt.target.value;
 
-        update(name, value);
+        updateForm(inputName, inputValue);
     }
 
     const onSubmit = evt => {
         evt.preventDefault();
-        submit();
+        submitForm();
     }
 
     return (
         <form className='form container' onSubmit={onSubmit}>
           <div className='form-group inputs'>
 
-            <label>Member Name
+            <label>Name
                 <input
                     name='name'  // this name has to be exactly the same as label name
                     type='text'
                     placeholder='Type your name here'
                     maxLength='50'
-                    value={values.name}
+                    value={formValues.name}
                     onChange={onChange}
                 />
             </label>
@@ -35,22 +36,22 @@ export default function MemberForm(props) {
                     name='email'  // this name has to be exactly the same as label name
                     type='email'
                     placeholder='Type your email here'
-                    value={values.email}
+                    value={formValues.email}
                     onChange={onChange}
                 />
             </label>
     
             <label>Role
-                <select value={values.role} name='role' onChange={onChange}>
+                <select value={formValues.role} name='role' onChange={onChange}>
                     <option value=''>-- Select a Role --</option>
                     <option value='New Member'>New Member</option>
                     <option value='Current Member'>Current Member</option>
-                    <option value='Just Visiting'>Just Visiting</option>
+                    <option value='Other'>Other</option>
                 </select>
             </label>
     
             <div className='submit'>
-              <button disabled={!values.username || !values.email || !values.role}>submit</button> {/** makes it so submit button is disabled if any of these are not filled out */}
+              <button>SUBMIT</button>
             </div>
           </div>
         </form>
